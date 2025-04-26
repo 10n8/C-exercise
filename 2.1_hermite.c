9fff4e7592df15516d90eddfbdeb5587cc152baf
10n8 <junruk4696@gmail.com>
2025-04-26 18:36:03 +0900

    2.1_hermite perfect!

--- 2.1_hermite.c
+++ 2.1_hermite.c
@@ -2,20 +2,22 @@
 #include <math.h>
 double hermite(int n, double x);
 double legendre(int n, double x);
+double hermite_5(double x);
+double legendre_5(double x);
 
 int main()
 {
     int n, i;
-    double hv, lv;
+    double hv, lv, hv_5, lv_5;
     printf("\n Hermite and Legendre polynomial n ==> ");
     scanf("%d", &n);
-    //printf("\n variable x ==> ");
-    //scanf("%lf", &x);
     for (i=0; i<=20; i++)
     {
         hv = hermite(n, 0.05*i);
         lv = legendre(n, 0.05*i);
-        printf("\n x = %5.3f : hv = %15.8e, lv = %15.8e", 0.05*i, hv, lv);
+        hv_5 = hermite_5(0.05*i);
+        lv_5 = legendre_5(0.05*i);
+        printf("\n x = %5.3f : hv = %5.3e (%5.3e) lv = %5.3e (%5.3e)", 0.05*i, hv, hv_5, lv, lv_5);
     }
 }
 
@@ -44,12 +46,22 @@ double legendre(int n, double x)
     if (n==0)
     return y0;
     y1=x;
-    for (k=2; k<=n; k++);
+    for (k=2; k<=n; k++)
     {
         double l = (double)k;
-        y2=((2.*k-1)*x*y1-(k-1)*y0)/l;
+        y2=((2.*l-1)*x*y1-(l-1)*y0)/l;
         y0=y1;
         y1=y2;
     }
     return y1;
+}
+
+double hermite_5(double x)
+{
+    return 32. * x * x * x * x * x - 160.* x * x * x + 120.*x;
+}
+
+double legendre_5(double x)
+{
+    return (63. * x * x * x * x * x - 70. * x * x * x + 15. * x)/8.;
 }
